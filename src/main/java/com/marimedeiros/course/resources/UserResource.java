ea34e2f68c39a6ebbd09b2189cc0e026676e0e21
Mariana Medeiros <mariana_medeiros.santos@outlook.com>
2025-05-13 09:53:43 -0300

    User entity and resource

---
+++ src/main/java/com/marimedeiros/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.marimedeiros.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marimedeiros.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Mariana,", "mariana@hotmail.com", "11940780652", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
